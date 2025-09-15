5d1cf93224b704b1ff68d33f34eec4706781ca82
Johan Daniel Aguirre Arias <aguirreariasjohandaniel@gmail.com>
2025-09-14 21:35:06 -0500

    feat: add the code to make a vm that can be connected using ssh

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,15 @@ output "url" {
   sensitive   = false
   description = "description"
 }
+output "vm_public_ip" {
+  description = "Public IP of the Linux VM"
+  value       = azurerm_public_ip.vm_public_ip.ip_address
+}
+
+output "ssh_access_enabled" {
+  description = "Indica si el puerto 22 está permitido en el NSG"
+  value = [
+    for rule in azurerm_network_security_group.nsg.security_rule : rule.name
+    if rule.destination_port_range == "22" && rule.access == "Allow"
+  ]
+}
