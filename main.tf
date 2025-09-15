841237b902a87372495fe3c3247d90a94682bf04
Johan Daniel Aguirre Arias <aguirreariasjohandaniel@gmail.com>
2025-09-14 21:24:11 -0500

    feat: add the code to make a vm that can be connected using ssh

--- main.tf
+++ main.tf
@@ -83,4 +83,63 @@ resource "azurerm_function_app_function" "faf" {
   })
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-linux"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-linux"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "${var.vm_name}-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  disable_password_authentication = false
+}
--- variables.tf
+++ variables.tf
@@ -8,3 +8,23 @@ variable "location" {
   default     = "mexicocentral"
   description = "Location"
 }
+
+variable "vm_name" {
+  type    = string
+  default = "mi-vm-linux"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  type    = string
+  default = "P@ssw0rd1234!" # OJO: Para pruebas. Lo ideal es usar SSH keys
+}
